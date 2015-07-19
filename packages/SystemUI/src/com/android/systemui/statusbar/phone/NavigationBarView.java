2d586765667f7610a0d66526394353c99332f22e
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-07-19 20:18:56 +0200

    SlimDim: Use semaphore to cancel ontouch callback
    Only cancel callback if there actually is a callback pending
    
    PS3 : forgot to set semaphore to false in the runnable, once it's
          calledback, it's not scheduled anymore, no need to cancel it.
    
    Change-Id: I205ccadb4afcf0f8ef4157b666d0df6161132c91
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -114,6 +114,7 @@ public class NavigationBarView extends LinearLayout {
     private boolean mDimNavButtonsAnimate;
     private int mDimNavButtonsAnimateDuration;
     private boolean mDimNavButtonsTouchAnywhere;
+    private boolean mIsHandlerCallbackActive = false;
 
     private NavigationBarViewTaskSwitchHelper mTaskSwitchHelper;
     private DeadZone mDeadZone;
@@ -224,7 +225,10 @@ public class NavigationBarView extends LinearLayout {
     };
 
     public void onNavButtonTouched() {
-        mHandler.removeCallbacks(mNavButtonDimmer);
+        if (mIsHandlerCallbackActive) {
+            mHandler.removeCallbacks(mNavButtonDimmer);
+            mIsHandlerCallbackActive = false;
+        }
         final boolean keyguardProbablyEnabled =
                 (mDisabledFlags & View.STATUS_BAR_DISABLE_HOME) != 0;
         if (getNavButtons() != null) {
@@ -237,6 +241,7 @@ public class NavigationBarView extends LinearLayout {
             }
             if (mDimNavButtons && !keyguardProbablyEnabled) {
                 mHandler.postDelayed(mNavButtonDimmer, mDimNavButtonsTimeout);
+                mIsHandlerCallbackActive = true;
             }
         }
     }
@@ -1042,6 +1047,7 @@ public class NavigationBarView extends LinearLayout {
     private Runnable mNavButtonDimmer = new Runnable() {
         @Override
         public void run() {
+            mIsHandlerCallbackActive = false;
             if (getNavButtons() != null && mIsDim == false) {
                 mIsDim = true;
                 if (mDimNavButtonsAnimate) {
